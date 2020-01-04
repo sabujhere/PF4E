2da22c8238878a35d30ebef62045c3fa3a4dc804
sabujhere <sabujhere@users.noreply.github.com>
2020-01-04 10:04:57 -0600

    ex 7.2

---
+++ Ex7.2.py
@@ -0,0 +1,11 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+sum = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    colonPosition = line.find(':')
+    count = count + 1
+    sum = sum + float(line[colonPosition + 1:].strip())
+print("Average spam confidence: ", sum/count)
